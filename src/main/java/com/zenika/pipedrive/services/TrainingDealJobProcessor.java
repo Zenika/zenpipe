2856c295afbc0edd03594769553c3168bd88528e
Pierre Nicoli <pierre.nicoli@gmail.com>
2023-01-03 08:12:57 +0100

    🚧 Building The Training Job processor

---
+++ src/main/java/com/zenika/pipedrive/services/TrainingDealJobProcessor.java
@@ -0,0 +1,11 @@
+package com.zenika.pipedrive.services;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class TrainingDealJobProcessor {
+	public void process(PipedriveTrainingDealJob job) {
+		// get the organization info
+		// update the fields
+	}
+}
